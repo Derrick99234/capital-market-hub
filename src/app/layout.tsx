a507a7e05c0f66affde42c1aca7f37b81aaf1181
Derrick <pshubomi@gmail.com>
2025-09-29 12:18:16 +0100

    :

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,6 @@ export default function RootLayout({
               new google.translate.TranslateElement(
                 {
                   pageLanguage: "en",
-                  includedLanguages: "en,fr,es,de,hy",
                   layout: google.translate.TranslateElement.InlineLayout.SIMPLE
                 },
                 "google_translate_element"
